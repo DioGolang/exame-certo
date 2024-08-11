8f79e1513a906bd827f5c000a72c443269c4a687
DioGoLang <di0g0vasconcelos@hotmail.com>
2024-08-10 23:23:28 -0300

    folder organization

---
+++ exame-certo-backend/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DomainModule } from './domain/domain.module';
+import { ApplicationModule } from './application/application.module';
+import { InfraModule } from './infra/infra.module';
+import { PresentationModule } from './presentation/presentation.module';
+
+@Module({
+  imports: [DomainModule, ApplicationModule, InfraModule, PresentationModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
