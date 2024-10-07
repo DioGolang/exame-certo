af52907fcc8c087067c71a082d2700a2f23f30fc
DioGoLang <di0g0vasconcelos@hotmail.com>
2024-10-06 23:47:48 -0300

    feat: Clinic Domain Service

--- exame-certo-backend/src/domain/services/clinic/clinic-domain-service.module.ts
+++ exame-certo-backend/src/domain/services/clinic/clinic-domain-service.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClinicDomainService } from './clinic-domain.service';
+import { BuildersModule } from '../../builders/builders.module';
 
 @Module({
+  imports: [BuildersModule],
   providers: [ClinicDomainService],
   exports: [ClinicDomainService],
 })
--- exame-certo-backend/src/domain/services/clinic/clinic-domain.service.ts
+++ exame-certo-backend/src/domain/services/clinic/clinic-domain.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { BuilderFactory } from '../../builders/builder.factory';
+import { CreateClinicDto } from '../../../application/clinic/dto/create-clinic.dto';
+import { Clinic } from '../../entities/clinic.entity';
+import { ClinicEntity } from '../../../infra/persistence/postgres/entities/clinic.entity';
 
 @Injectable()
-export class ClinicDomainService {}
+export class ClinicDomainService {
+  constructor(
+    @Inject('BuilderFactory')
+    private readonly clinicBuilderFactory: BuilderFactory,
+  ) {}
+
+  async createClinic(clinicDto: CreateClinicDto): Promise<Clinic> {
+    const clinicBuilder = await this.clinicBuilderFactory.createClinicBuilder(
+      undefined,
+      clinicDto.password,
+    );
+    return clinicBuilder
+      .withName(clinicDto.name)
+      .withEmail(clinicDto.email)
+      .withAddress(clinicDto.address)
+      .withContactInfo(clinicDto.contactInfo)
+      .build();
+  }
+
+  async rehydrateClinic(clinicEntity: ClinicEntity): Promise<Clinic> {
+    const clinicBuilder = await this.clinicBuilderFactory.createClinicBuilder(
+      clinicEntity.id,
+      clinicEntity.password,
+    );
+
+    const clinic = clinicBuilder
+      .withName(clinicEntity.name)
+      .withEmail(clinicEntity.email)
+      .withAddress(clinicEntity.address)
+      .withContactInfo(clinicEntity.contactInfo)
+      .build();
+
+    return clinic;
+  }
+}
